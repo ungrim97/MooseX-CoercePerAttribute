e88a52cfc8730699bada2561835ab767fecdb945
Mike Francis (mrf) <ungrim97@gmail.com>
2013-08-15 22:52:22 +0300

    Initial Commit on coversion to Dist::Zilla

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+MooseX::CoercePerAttribute - Blah blah blah
+
+# SYNOPSIS
+
+    use MooseX::CoercePerAttribute;
+
+# DESCRIPTION
+
+MooseX::CoercePerAttribute is
+
+# AUTHOR
+
+Mike Francis <ungrim97@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2013- Mike Francis
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/MooseX/CoercePerAttribute.pm
@@ -0,0 +1,39 @@
+package MooseX::CoercePerAttribute;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+MooseX::CoercePerAttribute - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use MooseX::CoercePerAttribute;
+
+=head1 DESCRIPTION
+
+MooseX::CoercePerAttribute is
+
+=head1 AUTHOR
+
+Mike Francis E<lt>ungrim97@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Mike Francis
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
